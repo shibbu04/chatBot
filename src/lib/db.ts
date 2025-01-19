add57978ab07fc2db093c842892780f078e7933b
shibbu04 <shivamsingh57680@gmail.com>
2025-01-20 04:01:19 +0530

    update supabase

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from '../types/supabase';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_POSTGRES_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase environment variables');
